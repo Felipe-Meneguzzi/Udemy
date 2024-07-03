ed915d173103afec9a4ab94a9e2d68f6148f483f
Felipe <felipe.meneguzzi@universo.univates.br>
2024-07-03 01:36:57 -0300

    2

--- main/src/section8/exercicioClasseStatica/CalculadoraDolar.java
+++ main/src/section8/exercicioClasseStatica/CalculadoraDolar.java
@@ -3,13 +3,14 @@ package section8.exercicioClasseStatica;
 public class CalculadoraDolar {
     private static final double IOF = 6;
 
-    public static double getIof(){
+    public static double getIof() {
         return IOF;
     }
 
-    public static double custoDolarComIOF(double precoDolar, double quantDolar){
+    public static double custoDolarComIOF(double precoDolar, double quantDolar) {
         double impostoIOF = (IOF / 100) * quantDolar;
-        return 1;
+        quantDolar += impostoIOF;
+        return quantDolar * precoDolar;
     }
 
 }
--- main/src/section8/exercicioClasseStatica/Main.java
+++ main/src/section8/exercicioClasseStatica/Main.java
@@ -2,6 +2,6 @@ package section8.exercicioClasseStatica;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(CalculadoraDolar.custoDolarComIOF(3.1, 200));
     }
 }
